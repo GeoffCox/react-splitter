95a1d228704fa157c6e5d195032a857a344ca352
Geoff Cox <geoff.cox@live.com>
2023-01-16 14:59:34 -0800

    Updated to latest dependencies

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loaders: ["awesome-typescript-loader"] },
+      { test: /\.tsx?$/, loader: "ts-loader" },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: "pre", test: /\.js?$/, loader: "source-map-loader" },
---
+++ package/dist/2.1.2/DefaultSplitter.d.ts
@@ -0,0 +1,12 @@
+import { RenderSplitterProps } from './RenderSplitterProps';
+import './defaultSplitter.css';
+type Props = RenderSplitterProps & {
+    color?: string;
+    hoverColor?: string;
+    dragColor?: string;
+};
+/**
+ * The default splitter which provides a thin line within a larger mouse hit area.
+ */
+export declare const DefaultSplitter: (props: Props) => JSX.Element;
+export {};
